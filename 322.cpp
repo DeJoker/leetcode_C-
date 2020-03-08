1a5def1598ed9a556f0844251cf1cb1afce7a37b
cxj <localjie@163.com>
2020-03-08 23:17:59 +0800

    [bug] oj heap buffer overflow, local show different(and is correct) result

--- 322.cpp
+++ 322.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <algorithm>
 using namespace std;
 
 template<typename Ty1, typename Ty2>
@@ -17,27 +18,39 @@ void DebugVecor(map<Ty1, Ty2>& mm)
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> combin(amount+1, -1);
+    // int max_int = 0xFFFFFFFF;
+    int max_int = 100000;
+        // for_each(coins.begin(), coins.end(),
+        //     [&](int coin)
+        //     {
+        //         if ()
+        //     }
+        // );
+        
+        vector<int> combin(amount+1, max_int);
         combin[0]=0;
         for(auto coin : coins)
         {
             combin[coin] = 1;
         }
-        // 初始化
-        // int it(1);
-        // for(; it<*coins.rbegin(); ++it)
+        // sort coin
+        // 如果coin大于amount没有处理
 
         for(int it=1; it<amount+1; ++it)
         {
-            int mincount(amount);
+            int mincount(max_int);
             for(auto coin : coins)
             {
                 if (it-coin < 0)
                     continue;
-                mincount = combin[it-coin]+1 < mincount ? combin[it-coin]+1 : mincount;
-                // if (combin[it-coin]+1 < mincount)
+                // mincount = combin[it-coin]+1 < mincount ? combin[it-coin]+1 : mincount;
+                if (combin[it-coin]+1 < mincount && combin[it-coin] != -1)
+                    mincount = combin[it-coin]+1;
             }
-            combin[it] = mincount;
+            if (mincount == max_int)
+                combin[it] = -1;
+            else
+                combin[it] = mincount;
         }
         return combin[amount];
     }
@@ -49,8 +62,12 @@ public:
 int main()
 {
     Solution sol;
-    vector<int> coins = {1,2,5};
-    int ret = sol.coinChange(coins, 10);
+    vector<int> coins = {2};
+    int ret = sol.coinChange(coins, 1);
+    // vector<int> coins = {1};
+    // int ret = sol.coinChange(coins, 0);
+    // vector<int> coins = {1,2,5};
+    // int ret = sol.coinChange(coins, 10);
     cout << ret << endl;
     return 0;
 }
