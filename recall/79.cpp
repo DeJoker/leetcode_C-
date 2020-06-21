daac2fbde4f7d518b459b7f73899c5e63402ea6c
cxj <localjie@163.com>
2020-06-22 01:01:12 +0800

    [mod] recall with change value

--- recall/79.cpp
+++ recall/79.cpp
@@ -55,41 +55,38 @@ public:
         m=board.size(); n=board[0].size();
         res = false;
         vector<pair<int,int>> starts;
-        set<pair<int,int>> visit;
-
         for(int i=0; i<m; ++i) {
             for(int j=0; j<n; ++j)
                 if (board[i][j] == word[0])
                     starts.push_back({i,j});
         }
+
         for(int i=0; i<starts.size(); ++i) {
             if (res){
                 return res;
             }
-            visit.insert(starts[i]);
-            dfs(board, word, 0, visit, starts[i]);
-            visit.clear(); // 忘记清理
+            dfs(board, word, 0, starts[i]);
         }
         return res;
     }
 
-    void dfs(vector<vector<char>>& board, string& word, int cnt, set<pair<int,int>>& visit, pair<int,int> pos) {
-        // if (pos.first<0 || pos.first>=m || pos.second<0 || pos.second>=n)
-        //     return;
+    // 回退好像不对劲
+    void dfs(vector<vector<char>>& board, string& word, int cnt, pair<int,int> pos) {
         if (cnt == word.size()-1) {
             res = true;
             return;
         }
-
+        char tmp = board[pos.first][pos.second];
+        board[pos.first][pos.second] = 0;
         static constexpr int dirt[4][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
         for(int i=0; i<4; ++i) {
             int nx = pos.first+dirt[i][0];
             int ny = pos.second+dirt[i][1];
             if (nx<0 || nx>=m || ny<0 || ny>=n)
                 continue;
-            if (word[cnt+1] == board[nx][ny] && visit.find({nx, ny}) == visit.end()) {
-                visit.insert({nx, ny});
-                dfs(board, word, cnt+1, visit, {nx, ny});
+            if (word[cnt+1] == board[nx][ny]) {
+                dfs(board, word, cnt+1, {nx, ny});
+                board[pos.first][pos.second] = tmp;
             }
         }
     }
@@ -103,6 +100,11 @@ int main() {
 
 
     vector<vector<char>> board3 = {{'C','A','A'},{'A','A','A'},{'B','C','D'}};
-    cout << Solution().exist(board3, "AAB") <<endl;
+    // cout << Solution().exist(board3, "AAB") <<endl;
 
+// [["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]]
+// "ABCESEEEFS"
+
+    vector<vector<char>> board23 = {{'A','B','C','E'},{'S','F','E','S'},{'A','D','E','E'}};
+    cout << (Solution().exist(board23, "ABCESEEEFS") ? "true" : "false") << endl;
 }
