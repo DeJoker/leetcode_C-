1615b37e23a58b9883844ca87f4d6acf77369327
cxj <18501966+DeJoker@users.noreply.github.com>
2020-12-05 00:43:31 +0800

    30 没有用上map搜索导致超时

--- 未分类/30.串联所有单词的子串.cpp
+++ 未分类/30.串联所有单词的子串.cpp
@@ -9,6 +9,9 @@
 // 第一，没有移动i指针      没写+= word.size()
 // 插入值错误，内部循环应该有一个下标，实际插入应该是i，而向前移动应该是k
 // 没有考虑vector中可能有重复元素就放入set，应该用map
+// <= 的边界条件   因为加了长度， 0+长度就是末尾
+// 都用map了为什么不上搜索？？
+
 
 // @lc code=start
 class Solution {
@@ -16,23 +19,28 @@ public:
     vector<int> findSubstring(string& s, vector<string>& words) {
         vector<int> res;
         int len(0);
+        map<string, int> patternWord;
         for(int i=0; i<words.size(); ++i) {
             len += words[i].size();
+            patternWord[words[i]]++;
         }
         if (s.size() < len) {
             return res;
         }
 
-        for(int i=0; i+len<s.size(); ++i) {
+        for(int i=0; i+len<=s.size(); ++i) { // 这里为什么是<=
             bool match(false);
-            set<string> pattern = {words.begin(), words.end()};
+            map<string, int> pattern(patternWord);
             int k(i);
             do {
                 match = false;
-                for(auto& word : pattern) {
+                for(auto& [word,cnt] : pattern) {
                     if (s.substr(k, word.size()) == word) {
                         match = true;
-                        pattern.erase(word);
+                        pattern[word]--;
+                        if (pattern[word] == 0) {
+                            pattern.erase(word);
+                        }
                         k += word.size();
                         break;
                     }
@@ -50,7 +58,10 @@ public:
 
 
 int main() {
-    string s("barfoothefoobarman");
-    vector<string> vec = {"foo","bar"};
+    // string s("barfoothefoobarman");
+    // vector<string> vec = {"foo","bar"};
+
+    string s("wordgoodgoodgoodbestword");
+    vector<string> vec = {"word","good","best","good"};
     DebugVector( Solution().findSubstring(s, vec) );
 }
