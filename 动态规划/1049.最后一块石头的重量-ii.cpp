b8463fc53a2e433055fc90685cb7fe1a93d4f202
cyj <cuiyongjie@cvte.com>
2021-03-22 16:21:19 +0000

    [add] 01背包 最后石头重量

--- 动态规划/1049.最后一块石头的重量-ii.cpp
+++ 动态规划/1049.最后一块石头的重量-ii.cpp
@@ -6,11 +6,32 @@
 #include "../common/debugger.h"
 
 // @lc code=start
+
+// 分为两组，组间差距最小即可
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
+        int sum(0);
+        for(int s : stones) sum += s;
+        int target = sum/2;
 
+        vector<int> dp(target+1);
+        for(int i=0; i<stones.size(); i++) {
+            for(int j=target; j>=stones[i]; --j) {
+                dp[j] = max(dp[j], dp[j-stones[i]]+stones[i]);
+            }
+        }
+        // dp[target] 是最大容纳石头个数
+        // 两堆分别是 dp[target] 和 sum - dp[target] 两者相减就是差距
+        return (sum - dp[target]) - dp[target];
     }
 };
 // @lc code=end
 
+
+int main() {
+    vector<int> p;
+
+    p={2,7,4,1,8,1};
+    LOG_DEBUG << Solution().lastStoneWeightII(p);
+}
