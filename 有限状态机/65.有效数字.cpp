b199c630cb1c283f560d697610a6387ab8c2a5f7
cyj <cuiyongjie@cvte.com>
2021-04-23 09:29:05 +0000

    [fix] 修复部分用例，仍然有误

--- 有限状态机/65.有效数字.cpp
+++ 有限状态机/65.有效数字.cpp
@@ -15,31 +15,46 @@ public:
     bool isNumber(string str) {
         if (str.empty()) return false;
 
+        int i(1), n=str.size();
+        int value(-1); // 针对前导0的判断
+
         int curState;
         if (str[0]=='+' || str[0]=='-') {
             curState = 3;
         } else if (isdigit(str[0])) {
             curState = 0;
+            value = str[0]!='0';
         } else if (str[0] == '.') {
             curState = 1;
         } else {
             return false;
         }
 
-        int i(1), n=str.size();
+
+
         // 有限状态机
         //  第一维度  curState当前所处状态
         //  第二维度  当前下标对应的类别（四种）
-        vector<vector<bool>> state = {
+        static vector<vector<bool>> state = {
             {true, true, true, false},
             {true, false, false, false},
             {true, false, false, true},
             {true, true, false, false}, 
         };
+        
+        // 只有一位
+        if (n==1 && curState != 0) {
+            return false;
+        }
 
         while(i<n) {
             if (isdigit(str[i])) {
                 curState = 0;
+                if (str[i]!='0') {
+                    value = 1;
+                } else if (value==-1) {
+                    value = 0;
+                }
             } else if (str[i]=='+' || str[i]=='-') {
                 if (!state[curState][3]) {
                     return false;
@@ -48,18 +63,27 @@ public:
             } else if (str[i] == 'e' || str[i] == 'E') {
                 if (!state[curState][2]) {
                     return false;
+                } 
+                if (value!=1) {
+                    return false;
                 }
                 curState = 2;
+                value = -1;
             } else if (str[i] == '.'){
                 if (!state[curState][1]) {
                     return false;
                 }
                 curState = 1;
+                value = -1;
             } else {
                 return false;
             }
 
             ++i;
+        }
+
+        if (str.back() == 'e') {
+            return false;
         }
 
         return true;
@@ -70,13 +94,22 @@ public:
 int main() {
     LOG_DEBUG << Solution().isNumber("e"); // false
 
-    
+
     LOG_DEBUG << Solution().isNumber("0"); // true
-    LOG_DEBUG << Solution().isNumber("."); // true
     LOG_DEBUG << Solution().isNumber(".1"); // true
     LOG_DEBUG << Solution().isNumber("-123.456e789"); // true
     LOG_DEBUG << Solution().isNumber("0089"); // true
     LOG_DEBUG << Solution().isNumber("-.9"); // true
     LOG_DEBUG << Solution().isNumber("53.5e93"); // true
+
+
+    // // 未通过一下用例
+    LOG_DEBUG << Solution().isNumber("."); // false
+
+    LOG_DEBUG << Solution().isNumber("0e"); // false  前导0问题？？？
+    // e要求前后都有值
+    LOG_DEBUG << Solution().isNumber("5e"); // false
+    // 只能有一个小数点？   1422/1488 cases passed (N/A)
+    LOG_DEBUG << Solution().isNumber(".1."); // false
 }
 
